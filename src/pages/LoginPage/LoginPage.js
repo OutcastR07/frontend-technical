6bb926aff3aa97882ee1d4ff7e1d433682d2a57a
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-06-08 11:36:37 +0600

    login-validation

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -5,10 +5,50 @@ import {
   HelpOutline,
   LinkedIn,
   LockOpen,
+  Visibility,
+  VisibilityOff,
 } from "@mui/icons-material";
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const validateEmail = (input) => {
+  // Email validation regular expression
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(input);
+};
+
+const validatePassword = (input) => {
+  // Password validation regular expression
+  const passwordRegex =
+    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=()])(?=\S+$).{8,20}$/;
+  return passwordRegex.test(input);
+};
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = () => {
+    // Perform input validation
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      setError(
+        "Password must be 8 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character"
+      );
+      return;
+    }
+
+    // Navigate to home route
+    navigate("/");
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
       <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
@@ -27,28 +67,39 @@ const LoginPage = () => {
               type="text"
               placeholder="Email Address"
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full outline-none"
             />
             <label className="font-semibold text-sm text-gray-600 pb-1 block">
               Password
             </label>
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full outline-none"
-            />
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full outline-none"
+              />
+              <span
+                className="absolute right-3 top-3 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </span>
+            </div>
+            {error && <p className="text-red-500 mb-3">{error}</p>}
             <button
               type="button"
-              data-te-ripple-init
-              data-te-ripple-color="light"
               className="transition duration-200 bg-blue-500 hover:bg-blue-600 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+              onClick={handleLogin}
             >
               <span className="inline-block mr-2">Login</span>
             </button>
           </div>
           <div className="p-5 flex justify-between gap-x-3">
-            {" "}
             <button
               type="button"
               className="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block"
